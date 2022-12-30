321380322f06c9304341d9cbed5a97d9190e558e
Vincent Biret <vibiret@microsoft.com>
2022-12-30 17:18:51 -0500

    - aligns structure with v1
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ lib/microsoft_graph_service_client.rb
@@ -0,0 +1,7 @@
+require_relative 'microsoft_graph_beta/microsoft_graph_base_service_client'
+module MicrosoftGraphBeta
+	class MicrosoftGraphServiceClient < MicrosoftBaseGraphServiceClient
+		#TODO define sugar constructors acception the authentication provider
+		#TODO shim me
+	end
+end
