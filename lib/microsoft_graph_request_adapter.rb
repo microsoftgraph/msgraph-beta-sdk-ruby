f7a7e0e79869b7057e21c322b87d284311835fc0
Vincent Biret <vibiret@microsoft.com>
2023-01-03 13:22:41 -0500

    - minor fixes to manual files

--- lib/microsoft_graph_request_adapter.rb
+++ lib/microsoft_graph_request_adapter.rb
@@ -5,7 +5,7 @@ module MicrosoftGraphBeta
 	class MicrosoftGraphRequestAdapter < MicrosoftGraphCore::GraphRequestAdapterBase
 		def initialize(authentication_provider, client_options=MicrosoftGraphCore::GraphClientOptions.new, parse_node_factory=MicrosoftKiotaAbstractions::ParseNodeFactoryRegistry.default_instance, serialization_writer_factory=MicrosoftKiotaAbstractions::SerializationWriterFactoryRegistry.default_instance, client = nil)
 			options = MicrosoftGraphCore::GraphClientOptions.new
-			options.graph_service_library_version = "v" + MicrosoftGraph::VersionInformation::VERSION
+			options.graph_service_library_version = "v" + MicrosoftGraphBeta::VersionInformation::VERSION
 			options.graph_service_version = "beta"
 			super(authentication_provider, options, parse_node_factory, serialization_writer_factory, client)
 		end
--- lib/microsoft_graph_service_client.rb
+++ lib/microsoft_graph_service_client.rb
@@ -1,6 +1,6 @@
 require_relative 'microsoft_graph_beta/microsoft_graph_base_service_client'
 module MicrosoftGraphBeta
-	class MicrosoftGraphServiceClient < MicrosoftBaseGraphServiceClient
+	class MicrosoftGraphServiceClient < MicrosoftGraphBeta::MicrosoftBaseGraphServiceClient
 		#TODO define sugar constructors acception the authentication provider
 		#TODO shim me
 	end
