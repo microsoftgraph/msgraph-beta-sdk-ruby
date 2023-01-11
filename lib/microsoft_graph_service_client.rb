49c579cf4cb225fc6ce511e9e064b4061620c472
Vincent Biret <vibiret@microsoft.com>
2023-01-11 15:34:09 -0500

    - import name fix

--- lib/microsoft_graph_service_client.rb
+++ lib/microsoft_graph_service_client.rb
@@ -1,6 +1,6 @@
-require_relative 'microsoft_graph_beta/microsoft_graph_base_service_client'
+require_relative 'microsoft_graph_base_service_client'
 module MicrosoftGraphBeta
-	class MicrosoftGraphServiceClient < MicrosoftGraphBeta::MicrosoftBaseGraphServiceClient
+	class MicrosoftGraphServiceClient < MicrosoftGraphBeta::MicrosoftGraphBaseServiceClient
 		#TODO define sugar constructors acception the authentication provider
 		#TODO shim me
 	end
